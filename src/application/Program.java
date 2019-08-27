f7d585c9cb363dd1fe5a8e5302cc5f1bd3a14762
cristianomaf <cristianomaf@live.com>
2019-08-26 21:07:31 -0300

    Criando Classe dentro do programa princ e usando o sort nela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Produto;
@@ -16,8 +16,20 @@ public class Program {
 		lista.add(new Produto("Notebbok",1200.00));
 		lista.add(new Produto("Tablet",450.00));
 		
+		
+		//criando uma classe anonima ignorando a classe myComparator criada anteriormente
+		Comparator<Produto> comp = new Comparator<Produto>() {
+
+			@Override
+			public int compare(Produto o1, Produto o2) {
+				
+				return o1.getNome().toUpperCase().compareTo(o2.getNome().toUpperCase());
+			}
+			
+		};
 		//ordena a lista usando metodo sort da classe List
-		lista.sort(new MyComparator());		
+		
+		lista.sort(comp);		
 	
 		//percorre a lista e imprime os obejtos p da lista
 		for(Produto p : lista) {
